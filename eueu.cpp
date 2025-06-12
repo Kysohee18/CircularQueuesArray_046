0399f310ea4e559e963cd95b9121190bf7ae576f
Kyosohee18 <hanashahky@gmail.com>
2025-06-12 11:04:05 +0700

    acces mofifier

--- eueu.cpp
+++ eueu.cpp
@@ -6,5 +6,11 @@ using namespace std;
 class Queues {
     int FRONT, REAR, max = 5;
     int queue_array[5];
+    
+    public: // acces modifier
+    Queues(){
+        FRONT = -1;
+        REAR -= -1;
+    }
 
 
